f4cb8ee3144c23236a9d9371ba44cdb795598f6a
ADIL NAIMI <adilnm23@hotmail.com>
2019-06-20 07:09:11 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,10 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks/new' do
+
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark=Landmark.create(params[:landmark])
+  end
 end

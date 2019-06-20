a10552a1e64fca6deb8daadc1823934e29f621a5
ADIL NAIMI <adilnm23@hotmail.com>
2019-06-20 07:08:54 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -51,16 +51,5 @@ class FiguresController < ApplicationController
 
   end
 
-  get '/landmarks/new' do
-
-    erb :'landmarks/new'
-  end
-
-  post '/landmarks' do
-    @landmark=Landmark.create(params[:landmark])
-  end
-
-
-
 
 end

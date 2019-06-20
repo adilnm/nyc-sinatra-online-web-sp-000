e837194d949da6b4ac4d7ce8a35a7127ae75ba2a
ADIL NAIMI <adilnm23@hotmail.com>
2019-06-20 04:51:10 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -29,8 +29,10 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/:id/edit' do
-    @figure = Figure.find(params[:id])
-    erb :'figures/edit'
+    @figure=Figure.find(params[:id])
+    @titles=Title.all
+    @landmarks=Landmark.all
+    erb :'/figures/edit'
   end
 
   patch '/figures/:id' do
